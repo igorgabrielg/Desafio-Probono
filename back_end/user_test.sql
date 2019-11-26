94e0a3ffc0e43afd74dbde0bef9d67e850a271d2
Igor Gabriel <igor.gabrielg@gmail.com>
2019-11-25 23:58:15 -0300

    Atualização das tabelas do db

--- back_end/user_test.sql
+++ back_end/user_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24-Nov-2019 às 08:41
+-- Tempo de geração: 26-Nov-2019 às 03:57
 -- Versão do servidor: 10.4.8-MariaDB
 -- versão do PHP: 7.3.11
 
@@ -34,6 +34,14 @@ CREATE TABLE `tbl_sessions` (
   `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `tbl_sessions`
+--
+
+INSERT INTO `tbl_sessions` (`session_id`, `expires`, `data`) VALUES
+('FOVMu3Tc5Gr5WOcsIV1xtX-t4OTZwKO5', 1574722758, '{\"cookie\":{\"originalMaxAge\":600000,\"expires\":\"2019-11-25T22:50:07.812Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"passport\":{\"user\":18}}'),
+('eMfZQ8zTieJzU5T5hKSalWWFw3PlnO7K', 1574737037, '{\"cookie\":{\"originalMaxAge\":600000,\"expires\":\"2019-11-26T02:48:45.454Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"passport\":{\"user\":18}}');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +52,9 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
   `salt` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -51,11 +62,10 @@ CREATE TABLE `users` (
 -- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `salt`) VALUES
-(1, 'admin', '44fbd516263fad9624dba8b355dd54ca31cebaff1a6153a24f4945ba01724460da5592e4141e0b20211d86b8d6fb46b48f3d53962af797052f264a23058f53e9', '5ad4d580621a0f11'),
-(3, 'admin', '67a7184504e347fe3b72b04c4b29c96d234d87674c99cb543dcaf769abecdd878c8e208ca6be8e5504de2f53ab83dcf6807fd0ae7496490771a999520e4d90bb', '6e5ca69156e5efdd'),
-(4, 'admin', '820828d0c505c26d57d50d184a079ca562a555612ae8df753acfcffdda7838d6f67823e5576efa394e6124e23820a05b6f01fbc25bcaded3f6da018be60828ab', '0eb4de4328b106ee'),
-(5, 'admin', '27a5412744a034171c75d37003b5f49cb3a16b45d7ba9ba706cc51e12ba0f79a7ad7c96a76e9bcafa599144fea74792c86a55d250f9154f8f4f86dce0dfa2d2c', '6ed3826da3537bce');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `name`, `cpf`, `salt`) VALUES
+(18, 'admin', '5f91a6fa2cfda9e7c9a42b5239247c548ddc37369efe44880a14dbc93e26eb02a6aa4a7c61fca62e9e94624f3861105a5146cfa20aea25091e284bc9c9ae5ad3', 'admin@admin.com', 'admin', '000.000.000-55', 'd906e1f1e0fba9bd'),
+(19, 'admin2', 'c8e73173af67f8cbb2a7e89ebd01af814ff0fb5a8569885e6a415c3e5fa946871a4a8975f21f098e1606744590e1b94764d047792f2c818d891df132c4497d92', 'Igor.gabrielg@gmail.com', 'admin2', '008.263.381-94', 'ea3a645d3d872550'),
+(20, 'gfdgfd', 'a9eb49e637ca7b351a4d4938c1fde8a6a2b2eace7984f35fba146e87a6cbcacf59d9d335e59e40d18b557f0d2bd15b3dd66207cd3ff90d6815f82660ad8c6c61', 'Igor.gabrielg@gmail.com', 'igorasdsad', '000.000.000-59', 'a4ff127da9cebb8d');
 
 --
 -- Índices para tabelas despejadas
@@ -71,7 +81,9 @@ ALTER TABLE `tbl_sessions`
 -- Índices para tabela `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `salt` (`email`),
+  ADD KEY `salt_2` (`email`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -81,7 +93,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
